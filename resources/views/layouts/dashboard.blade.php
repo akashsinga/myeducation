720ae65b248f6d9ec5da840cddf6f15f777cecfb
Akash Singa <akashsinga@gmail.com>
2020-05-30 19:28:24 +0530

    local styles and js

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,10 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="csrf-token" content="EDzjkS4GBXADwTQJ5fdABDXpDEm8tjd8gaJhN0Tp">
     <title>@yield('title')</title>
-    <link rel="apple-touch-icon" sizes="76x76"
-        href="https://material-dashboard-laravel.creative-tim.com/material/img/apple-icon.png">
-    <link rel="icon" type="image/png"
-        href="https://material-dashboard-laravel.creative-tim.com/material/img/favicon.png">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
         name='viewport' />
     <!--     Fonts and icons     -->
@@ -18,17 +14,17 @@
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
-    <link href="https://material-dashboard-laravel.creative-tim.com/material/css/material-dashboard.css?v=2.1.3"
+    <link href="../assets/css/material-dashboard.css"
         rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="https://material-dashboard-laravel.creative-tim.com/material/demo/demo.css" rel="stylesheet" />
+    <link href="../assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="">
     <form id="logout-form" action="#" method="POST" style="display: none;">
         <input type="hidden" name="_token" value="EDzjkS4GBXADwTQJ5fdABDXpDEm8tjd8gaJhN0Tp"> </form>
     <div class="wrapper ">
-        <div class="sidebar" data-color="orange" data-background-color="white">
+        <div class="sidebar" data-color="orange" data-background-color="purple">
             <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
@@ -48,92 +44,27 @@
                         </a>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" data-toggle="collapse" href="#managestudents" aria-expanded="true">
+                        <a class="nav-link" href="#">
                             <i class="material-icons">person</i>
-                            <p>Manage Students
-                                <b class="caret"></b>
-                            </p>
+                            <p>Manage Students</p>
                         </a>
-                        <div class="collapse" id="managestudents">
-                            <ul class="nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">View Students</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Add Students</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Update Student</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" data-toggle="collapse" href="#managefaculty" aria-expanded="true">
+                        <a class="nav-link" href="#">
                             <i class="material-icons">person</i>
-                            <p>Manage Faculty
-                                <b class="caret"></b>
-                            </p>
+                            <p>Manage Faculty</p>
                         </a>
-                        <div class="collapse" id="managefaculty">
-                            <ul class="nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">View Faculty</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Add Faculty</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Update Faculty</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" data-toggle="collapse" href="#manageclassrooms" aria-expanded="true">
-                            <i class="material-icons">tab</i>
-                            <p>Manage Classrooms
-                                <b class="caret"></b>
-                            </p>
+                        <a class="nav-link" href="#">
+                            <i class="material-icons">airplay</i>
+                            <p>Manage Classrooms</p>
                         </a>
-                        <div class="collapse" id="manageclassrooms">
-                            <ul class="nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">View Classrooms</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Add Classroom</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Update Classroom</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
                     <li class="nav-item ">
                         <a class="nav-link" data-toggle="collapse" href="#reports" aria-expanded="true">
-                            <i class="material-icons">assignment</i>
-                            <p>Reports
-                                <b class="caret"></b>
-                            </p>
+                            <i class="material-icons">analytics</i>
+                            <p>Reports</p>
                         </a>
                         <div class="collapse" id="reports">
                             <ul class="nav">
@@ -156,31 +87,10 @@
                         </div>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" data-toggle="collapse" href="#subjects" aria-expanded="true">
+                        <a class="nav-link" href="#">
                             <i class="material-icons">library_books</i>
-                            <p>Manage Subjects
-                                <b class="caret"></b>
-                            </p>
+                            <p>Manage Subjects</p>
                         </a>
-                        <div class="collapse" id="subjects">
-                            <ul class="nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">View Subjects</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Add Subject</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <span class="sidebar-normal">Update Subject</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">
@@ -192,12 +102,6 @@
                         <a class="nav-link" href="#">
                             <i class="material-icons">calendar_today</i>
                             <p>Academic Schedule</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="material-icons">library_books</i>
-                            <p>Manage Subjects</p>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -324,94 +228,56 @@
     </div>
 
     <!--   Core JS Files   -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/core/jquery.min.js"></script>
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/core/popper.min.js"></script>
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/core/bootstrap-material-design.min.js">
+    <script src="../assets/js/core/jquery.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap-material-design.min.js">
     </script>
-    <script
-        src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/perfect-scrollbar.jquery.min.js">
+    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js">
     </script>
     <!-- Plugin for the momentJs  -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/moment.min.js"></script>
+    <script src="../assets/js/plugins/moment.min.js"></script>
     <!--  Plugin for Sweet Alert -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/sweetalert2.js"></script>
+    <script src="../assets/js/plugins/sweetalert2.js"></script>
     <!-- Forms Validations Plugin -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/jquery.validate.min.js">
+    <script src="../assets/js/plugins/jquery.validate.min.js">
     </script>
     <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/jquery.bootstrap-wizard.js">
+    <script src="../assets/js/plugins/jquery.bootstrap-wizard.js">
     </script>
     <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/bootstrap-selectpicker.js">
+    <script src="../assets/js/plugins/bootstrap-selectpicker.js">
     </script>
     <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
-    <script
-        src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/bootstrap-datetimepicker.min.js">
+    <script src="../assets/js/plugins/bootstrap-datetimepicker.min.js">
     </script>
     <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/jquery.dataTables.min.js">
+    <script src="../assets/js/plugins/jquery.dataTables.min.js">
     </script>
     <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/bootstrap-tagsinput.js">
+    <script src="../assets/js/plugins/bootstrap-tagsinput.js">
     </script>
     <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/jasny-bootstrap.min.js">
+    <script src="../assets/js/plugins/jasny-bootstrap.min.js">
     </script>
     <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/fullcalendar.min.js"></script>
+    <script src="../assets/js/plugins/fullcalendar.min.js"></script>
     <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/jquery-jvectormap.js"></script>
+    <script src="../assets/js/plugins/jquery-jvectormap.js"></script>
     <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/nouislider.min.js"></script>
+    <script src="../assets/js/plugins/nouislider.min.js"></script>
     <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
     <!-- Library for adding dinamically elements -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/arrive.min.js"></script>
+    <script src="../assets/js/plugins/arrive.min.js"></script>
     <!--  Google Maps Plugin    -->
     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE'"></script>
     <!-- Chartist JS -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/chartist.min.js"></script>
+    <script src="../assets/js/plugins/chartist.min.js"></script>
     <!--  Notifications Plugin    -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/plugins/bootstrap-notify.js"></script>
+    <script src="../assets/js/plugins/bootstrap-notify.js"></script>
     <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="https://material-dashboard-laravel.creative-tim.com/material/js/material-dashboard.js?v=2.1.1"
+    <script src="../assets/js/material-dashboard.js"
         type="text/javascript"></script>
-    <script>
-        // Facebook Pixel Code Don't Delete
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window,
-            document, 'script', '//connect.facebook.net/en_US/fbevents.js');
-        try {
-            fbq('init', '111649226022273');
-            fbq('track', "PageView");
-        } catch (err) {
-            console.log('Facebook Track Error:', err);
-        }
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none" src="#" />
-    </noscript>
-    <script>
-        $(document).ready(function() {
-            // Javascript method's body can be found in assets/js/demos.js
-            md.initDashboardPageCharts();
-        });
-    </script>
 </body>
 
 </html>
