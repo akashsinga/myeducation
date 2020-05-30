6ce8e289695743157fa0bcedcdda3b33b19bca8d
Akash Singa <akashsinga@gmail.com>
2020-05-30 21:10:13 +0530

    manage students ui

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('./layouts.admin_dashboard')
+@section('title')
+    Admin | Dashboard
+@endsection
+@section('content')
+<h4>Title</h4>
+@endsection
---
+++ resources/views/admin/managestudents.blade.php
@@ -0,0 +1,112 @@
+@extends('./layouts.admin_dashboard')
+@section('title')
+Manage Students
+@endsection
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="button-group ml-auto">
+                    <a class="btn btn-info" href="#"><span><i class="material-icons">add</span></i>Add</a>
+                    <a class="btn btn-warning" href="#"><span><i class="material-icons">edit</i></span>Edit</a>
+                    <a class="btn btn-danger" href="#"><span><i class="material-icons">clear</i></span>Delete</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class=" text-primary">
+                                <th>
+                                    ID
+                                </th>
+                                <th>
+                                    Student Name
+                                </th>
+                                <th>
+                                    Department
+                                </th>
+                                <th>
+                                    Mobile
+                                </th>
+                                <th>
+                                    Email
+                                </th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        1
+                                    </td>
+                                    <td>
+                                        Dakota Rice
+                                    </td>
+                                    <td>
+                                        Niger
+                                    </td>
+                                    <td>
+                                        444444444444
+                                    </td>
+                                    <td class="text-primary">
+                                        something@something.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        2
+                                    </td>
+                                    <td>
+                                        Dakota Rice
+                                    </td>
+                                    <td>
+                                        Niger
+                                    </td>
+                                    <td>
+                                        444444444444
+                                    </td>
+                                    <td class="text-primary">
+                                        something@something.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        3
+                                    </td>
+                                    <td>
+                                        Dakota Rice
+                                    </td>
+                                    <td>
+                                        Niger
+                                    </td>
+                                    <td>
+                                        444444444444
+                                    </td>
+                                    <td class="text-primary">
+                                        something@something.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        4
+                                    </td>
+                                    <td>
+                                        Dakota Rice
+                                    </td>
+                                    <td>
+                                        Niger
+                                    </td>
+                                    <td>
+                                        444444444444
+                                    </td>
+                                    <td class="text-primary">
+                                        something@something.com
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
