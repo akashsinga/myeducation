00d007b15d779eb07d264fa74627d74f3d1a3737
Akash Singa <akashsinga@gmail.com>
2020-06-01 11:13:00 +0530

    layouts

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-// Route::group(['middleware'=>['auth','admin']],function(){
+//  Route::group(['middleware'=>['auth','admin']],function(){
     Route::get('/home',function () {
         return redirect('admin/dashboard');
     });
